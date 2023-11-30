01f09da22d7c6247cbec8f7f338c0b2b78a535bf
Ian Carbajal <iancarbajal2000@hotmail.com>
2023-11-29 19:26:29 -0600

    CASSANDRA

--- creacion-tablas-cas.sh
+++ creacion-tablas-cas.sh
@@ -1,25 +1,53 @@
 #! /bin/bash
 
-echo -e "\nCreamos tablas:"
+WORKING_DIR=$PWD
+
+echo -e "\nCreamos tablas cassandra:"
+
 
 docker exec -i cassandra cqlsh -e "CREATE keyspace spotify with  replication = {'class': 'SimpleStrategy','replication_factor': '1'};"
 
+docker exec -i cassandra cqlsh -e "use spotify;"
+docker exec -i cassandra cqlsh -e "drop table spotify.artists;drop table spotify.albums;drop table spotify.tracks;"
+
 echo "- Tabla artist"
-docker exec -i cassandra cqlsh -k spotify -e "create table artist(followers varchar(100), genre varchar(100),artist_id varchar(100),popularity varchar(100),name varchar(200));"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE artists (
+                                                followers varchar,
+                                                genre varchar,
+                                                artist_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                name varchar
+                                            );"
 
 echo "- Tabla album"
-docker exec -i cassandra cqlsh -k spotify -e "create table album(available_market varchar(100),album_id varchar(100),release_date varchar(100),total_tracks varchar(100),name varchar(200));"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE albums (
+                                                available_markets varchar,
+                                                album_id varchar PRIMARY KEY,
+                                                release_date varchar,
+                                                total_tracks varchar,
+                                                name varchar
+                                            );"
 
 echo "- Tabla track"
-docker exec -i cassandra cqlsh -k spotify -e "create table track(album_id varchar(100),available_market varchar(100),disc_number varchar(100),duration_ms varchar(100),explicit varchar(100), track_id varchar(50),popularity varchar(50),track_number varchar(50),name varchar(200));"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE tracks (
+                                                album_id varchar,
+                                                available_markets varchar,
+                                                disc_number varchar,
+                                                duration_ms varchar,
+                                                explicit varchar,
+                                                track_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                track_number varchar,
+                                                name varchar
+                                            );"
 
 echo -e '\nInsertamos datos en tablas:'
 
 echo '- Artistas'
-docker exec -i cassandra cqlsh -k spotify -e "copy offset 2 into artist from '/data/monet/artists_mon.csv' on client using delimiters ',',E'\n',E'\"' null as ' ';"
+cat $WORKING_DIR/csv/cas/artists-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY artists FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
 
 echo '- Albums'
-docker exec -i cassandra cqlsh -k spotify -e "copy offset 2 into album from '/data/monet/albums_mon.csv' on client using delimiters ',',E'\n',E'\"' null as ' ';"
+cat $WORKING_DIR/csv/cas/albums-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY albums FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
 
 echo '- Tracks'
-docker exec -i cassandra cqlsh -k spotify -e "copy offset 2 into track from '/data/monet/tracks_mon.csv' on client using delimiters ',',E'\n',E'\"' null as ' ';"
+cat $WORKING_DIR/csv/cas/tracks-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY tracks FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
--- extract.sh
+++ extract.sh
@@ -78,3 +78,105 @@ cat $WORKING_DIR/csv/cas/tracks.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//
 rm $WORKING_DIR/csv/cas/artists.csv
 rm $WORKING_DIR/csv/cas/albums.csv
 rm $WORKING_DIR/csv/cas/tracks.csv
+
+#CASSANDRA
+
+echo -e "\nCreamos tablas cassandra:"
+
+
+docker exec -i cassandra cqlsh -e "CREATE keyspace spotify with  replication = {'class': 'SimpleStrategy','replication_factor': '1'};"
+
+docker exec -i cassandra cqlsh -e "use spotify;"
+docker exec -i cassandra cqlsh -e "drop table spotify.artists;drop table spotify.albums;drop table spotify.tracks;"
+
+echo "- Tabla artist"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE artists (
+                                                followers varchar,
+                                                genre varchar,
+                                                artist_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                name varchar
+                                            );"
+
+echo "- Tabla album"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE albums (
+                                                available_markets varchar,
+                                                album_id varchar PRIMARY KEY,
+                                                release_date varchar,
+                                                total_tracks varchar,
+                                                name varchar
+                                            );"
+
+echo "- Tabla track"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE tracks (
+                                                album_id varchar,
+                                                available_markets varchar,
+                                                disc_number varchar,
+                                                duration_ms varchar,
+                                                explicit varchar,
+                                                track_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                track_number varchar,
+                                                name varchar
+                                            );"
+
+echo -e '\nInsertamos datos en tablas:'
+
+echo '- Artistas'
+cat $WORKING_DIR/csv/cas/artists-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY artists FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
+
+echo '- Albums'
+cat $WORKING_DIR/csv/cas/albums-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY albums FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
+
+echo '- Tracks'
+cat $WORKING_DIR/csv/cas/tracks-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY tracks FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
+
+echo -e "\nCreamos tablas cassandra:"
+
+
+docker exec -i cassandra cqlsh -e "CREATE keyspace spotify with  replication = {'class': 'SimpleStrategy','replication_factor': '1'};"
+
+docker exec -i cassandra cqlsh -e "use spotify;"
+docker exec -i cassandra cqlsh -e "drop table spotify.artists;drop table spotify.albums;drop table spotify.tracks;"
+
+echo "- Tabla artist"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE artists (
+                                                followers varchar,
+                                                genre varchar,
+                                                artist_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                name varchar
+                                            );"
+
+echo "- Tabla album"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE albums (
+                                                available_markets varchar,
+                                                album_id varchar PRIMARY KEY,
+                                                release_date varchar,
+                                                total_tracks varchar,
+                                                name varchar
+                                            );"
+
+echo "- Tabla track"
+docker exec -i cassandra cqlsh -k spotify -e "CREATE TABLE tracks (
+                                                album_id varchar,
+                                                available_markets varchar,
+                                                disc_number varchar,
+                                                duration_ms varchar,
+                                                explicit varchar,
+                                                track_id varchar PRIMARY KEY,
+                                                popularity varchar,
+                                                track_number varchar,
+                                                name varchar
+                                            );"
+
+echo -e '\nInsertamos datos en tablas:'
+
+echo '- Artistas'
+cat $WORKING_DIR/csv/cas/artists-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY artists FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
+
+echo '- Albums'
+cat $WORKING_DIR/csv/cas/albums-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY albums FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
+
+echo '- Tracks'
+cat $WORKING_DIR/csv/cas/tracks-c.csv | docker exec -i cassandra cqlsh -k spotify -e "COPY tracks FROM stdin WITH DELIMITER=',' AND HEADER=TRUE;"
